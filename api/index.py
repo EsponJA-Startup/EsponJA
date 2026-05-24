bfbc2efd97788e5f671c6c8e6103b5074fee3333
Renzo <renzofilho2004z@gmail.com>
2026-05-23 22:53:17 -0300

    fix vercel python backend by moving entrypoint to server/index.py and using explicit builds

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Allow Python to find modules inside the /server subdirectory
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'server'))
-
-# Import the FastAPI app from your nested folder structure
-from main import app
